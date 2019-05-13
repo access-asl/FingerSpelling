a564076694b50de63b0290a90100550886fbe64c
BrowerDevelopments <jcb8561@rit.edu>
2019-05-13 19:03:47 -0400

    almost nie, nearly

--- Assets/Scripts/BoneObj.cs
+++ Assets/Scripts/BoneObj.cs
@@ -7,17 +7,20 @@ The bone object, will take two endpoints and then create
 a representation of a bone within the two spaces
 @author Brower
  */
-public class BoneObj
+public class BoneObj : MonoBehaviour
 {
+
+    public Vector3 origPos;
+    public Vector3 origScale;
     //The first joint
-    private Vector3 jointOne;
+    public Vector3 jointOne;
     //The second joint 
-    private Vector3 jointTwo;
-    //The Euler rotation of the bone
-    private Vector3 boneRotation;
+    public Vector3 jointTwo;
+    //The Quaternion rotation of the bone
+    public Quaternion boneRotation;
 
     //the distance between the two joints
-    private double length;
+    public float length;
     //The different types of rotation we can have default, Bottom
     public enum RotationType {
         Top,
@@ -25,9 +28,11 @@ public class BoneObj
         Bottom
     };
     //The specific rotation type of this Bone
-    private RotationType rotType;
+    public RotationType rotType;
     //When true it takes draws a line between the two digits for the bone
     public bool debug;
+
+    public Transform endPointOfLastBone;
 
     /*
     The specific constructor for a bone object where everything is defined
@@ -49,6 +54,17 @@ public class BoneObj
         this.length = Vector3.Distance(this.jointOne, this.jointTwo);
         this.rotType = RotationType.Bottom;
     }
+
+
+    public Vector3 GetJointOnePos() { return this.jointOne; }
+
+    public Vector3 GetJointTwoPos() { return this.jointTwo; }
+
+    public Quaternion GetBoneRotation() { return this.boneRotation; }
+
+    public float GetLength() { return this.length; }
+
+    public RotationType GetRotationType() { return this.rotType; }
 
     void OnDrawGizmos() {
         if (debug) {
--- Assets/Scripts/Bones.cs
+++ Assets/Scripts/Bones.cs
@@ -8,6 +8,7 @@ a class given to the bone object by way of
 inheritance form the initial start.
 Used to show rotation around the base of
 an object.
+Will be placed in the IKTarget object
 @author Brower
  */
 
@@ -16,15 +17,6 @@ public class Bones : MonoBehaviour
     //A bool that when true will show a gui representation of the current config for each bone
     public bool debugging;
 
-    //Holds refence to the type of base this bone has (currently the only option is bottom)
-    public enum BaseLocation {
-        Bottom
-    };
-
-    //The public refence of the base of the bone's location
-    public BaseLocation boneBase;
-
-
     //Holds the type of bone the current selection is
     public enum BoneType {
         Standard,
@@ -32,27 +24,84 @@ public class Bones : MonoBehaviour
         FK
     };
 
+    //Fill from root to leaf without having a parented armature
+    [Header("Animation Properties")]
+
+    [Tooltip("Root->Leaf only, all bones must be at the same level.")]
+    public BoneObj[] bones;
+
     //Creates a public reference to be edidted in the unity editor
     public BoneType boneType;
 
-    /**
-    Will calculate the bottom position of this bone
-    for  the initial creation will only work when the
-    bone's rotation as a Euler is (0, 0, 0)
-    */
-    public Vector3 getBottomCenter() {
-        Vector3 returnPos = Vector3.zero;
+    [Tooltip("Gives the bones a thing to look at in reference to the rest of the object")]
+    public Transform poleTarget;
+    [Tooltip("The object that moves the bone structure.")]
+    public Transform IKTarget;
+    //Stores the previous position of the target
+    private Vector3 previousTargetPos;
 
-        Mesh mesh = GetComponent<MeshFilter>().mesh;
-        Bounds bounds = mesh.bounds;
+    public Transform tipOfTheChain;
 
+    void Start() {
+        previousTargetPos = this.transform.position;
+        Initialize();
+    }
 
-        return returnPos;
+    void Update() {
+        if (previousTargetPos != this.transform.position) {
+            Solve();
+        }
+    }
+
+    void Initialize() {
+        bones[bones.Length-1].origPos = bones[bones.Length-1].transform.position;
+        bones[bones.Length-1].origScale = bones[bones.Length-1].transform.position;
+        bones[bones.Length-1].boneRotation = bones[bones.Length-1].transform.rotation;
+        GameObject g = new GameObject();
+        g.name = bones[bones.Length-1].transform.name;
+        g.transform.position = bones[bones.Length-1].transform.position;
+        g.transform.up = -(tipOfTheChain.position - bones[bones.Length-1].transform.position);
+        g.transform.parent = bones[bones.Length-1].transform.parent;
+        bones[bones.Length-1].transform.parent = g.transform;
+        for (int i = 0; i < bones.Length-1; i++) {
+            bones[i].origPos = bones[i].transform.position;
+            bones[i].origScale = bones[i].transform.localScale;
+            bones[i].boneRotation = bones[i].transform.rotation;
+            bones[i].length = Vector3.Distance(bones[i - 1].transform.position, bones[i].transform.position);
+            g = new GameObject();
+            g.name = bones[i].transform.name;
+            g.transform.position = bones[i].transform.position;
+            g.transform.up = -(bones[i - 1].transform.position - bones[i].transform.position);
+            g.transform.parent = bones[i].transform.parent;
+            bones[i].transform.parent = g.transform;
+        }
+    }
+
+    void Solve() {
+        Vector3 rootPos = bones[0].GetJointOnePos();
+        bones[0].transform.up = -(poleTarget.position - bones[0].transform.position);
+        for (int i = 1; i < bones.Length-1; i++) {
+            bones[i].transform.position = bones[i-1].transform.position + (-bones[i-1].transform.up * bones[i-1].GetLength());
+            bones[i].transform.up = -(poleTarget.position - bones[i].transform.position);
+        }
+        for (int i = 0; i < 5; i++) {
+            bones[bones.Length-1].transform.up = -(transform.position - bones[0].transform.position);
+            bones[bones.Length-1].transform.position = transform.position - (-bones[0].transform.up * bones[bones.Length-1].GetLength());
+            for (int j = bones.Length-2; j > 1; j--) {
+                bones[j].transform.up = -(bones[j+1].transform.position - bones[j].transform.position);
+                bones[j].transform.position = bones[j+1].transform.position - (-bones[j].transform.up * bones[j].GetLength());
+            }
+            bones[bones.Length - 1].transform.position = rootPos;
+            for (int j = 1; j < bones.Length - 1; j++) {
+                bones[j].transform.position = bones[j-1].transform.position + (-bones[j-1].transform.up * bones[j-1].GetLength());
+            }
+        }
+        previousTargetPos = transform.position;
     }
 
     void OnDrawGizmos() {
         Gizmos.color = new Color(1, 0, 0, 0.5f);
-        Gizmos.DrawCube(getBottomCenter(), new Vector3(.1f, .1f, .1f));
+        Gizmos.DrawCube(IKTarget.position, new Vector3(.1f, .1f, .1f));
+        Gizmos.DrawCube(poleTarget.position, new Vector3(.1f, .1f, .1f));
     }
-
 }
