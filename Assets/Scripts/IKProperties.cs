3488bc6b74672f0c05054c09f3a79e6d8a8248c9
BrowerDevelopments <jcb8561@rit.edu>
2019-05-13 10:09:54 -0400

    Initial Commit from new build

---
+++ Assets/Scripts/IKProperties.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+This class will store and help organize the final package of
+the IK Suite
+@author Brower
+ */
+public class IKProperties : MonoBehaviour
+{
+    
+}
