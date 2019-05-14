32e26fb79b30aba565e4f2644aab6675714bbdc2
BrowerDevelopments <jcb8561@rit.edu>
2019-05-13 23:12:56 -0400

    Rotation around bottom center of an object

---
+++ Assets/Scripts/IK_New/Bone.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+A new bone class will create a bone, and then use that to be able to
+rotate an object around the base instead of the middle.
+@author Brower
+ */
+public class Bone : MonoBehaviour
+{
+    private Vector3 bottom, top;
+    public Transform bone;
+
+    public Quaternion boneRotation;
+
+    void Start() {
+        bottom = calculateBottom();
+        top = calculateTop();
+    }
+
+    public Vector3 getTop() { return this.top; }
+
+    public Vector3 getBottom() { return this.bottom; }
+
+    public Transform getBone() { return this.bone; }
+
+    public void refresh() {
+        bottom = calculateBottom();
+        top = calculateTop();
+    }
+
+    private Vector3 calculateBottom() {
+        //The distance in a downward direction to go to hit the exact bottom of the object
+        float halfSize = bone.lossyScale.y/2;
+        return (-bone.up) * halfSize;
+    }
+
+    private Vector3 calculateTop() {
+        float halfSize = bone.localScale.y/2;
+        return (bone.up) * halfSize;
+    }
+
+    void OnDrawGizmos() {
+        Gizmos.color = new Color(1, 0, 0, 0.5f);
+        Gizmos.DrawCube(new Vector3(0, -.5f, 0), new Vector3(.1f, .1f, .1f));
+        Gizmos.DrawCube(transform.position, new Vector3(.1f, .1f, .1f));
+    }
+}
---
+++ Assets/Scripts/IK_New/InverseKinematics.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InverseKinematics : MonoBehaviour
+{
+    [System.Serializable]
+    public class BoneInformation {
+        [Tooltip("The array of bones Base->Tip base being the bone that hardly moves, tip being the bone that touches the aimTarget is at all possible.")]
+        public Bone[] bones;
+        public Transform poleTarget;
+        public Transform aimTarget;
+    }
+
+    public Quaternion test;
+
+    [Tooltip("This is where all bone properties are set.")]
+    public BoneInformation boneInformation;
+
+    void Update() {
+        rotateOnArc(test, boneInformation.bones[0]);
+    }
+
+    void rotateOnArc(Quaternion rotation, Bone bone) {
+        Vector3 startBottomPos = bone.getBottom();
+        bone.getBone().rotation = rotation;
+        bone.refresh();
+        // bone.rotation = rotation;
+        Vector3 newBottomPos = bone.getBottom();
+        Vector3 finalPos = new Vector3(0, 0, 0);
+        finalPos.x = -(newBottomPos.x - startBottomPos.x);
+        finalPos.y = -(newBottomPos.y - startBottomPos.y);
+        finalPos.z = -(newBottomPos.z - startBottomPos.z);
+        if (startBottomPos != newBottomPos) {
+            if (finalPos.x != 0 || finalPos.y != 0) {
+                Debug.Log(startBottomPos + " start");
+                Debug.Log(newBottomPos + " new");
+                Debug.Log(finalPos.x + " x");
+                Debug.Log(finalPos.y + " y");
+                Debug.Log(finalPos + " final");
+            }
+        }
+        bone.getBone().position += finalPos;
+    }
+
+    void OnDrawGizmos() {
+        Gizmos.color = new Color(0, 1, 1, .5f);
+        Gizmos.DrawCube(boneInformation.aimTarget.position, Vector3.one * .1f);
+        Gizmos.color = new Color(1, 1, 0, .5f);
+        Gizmos.DrawCube(boneInformation.poleTarget.position, Vector3.one * .1f);
+    }
+}
