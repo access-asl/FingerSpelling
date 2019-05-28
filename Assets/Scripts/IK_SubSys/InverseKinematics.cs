5e76a634c580ce7f01eb341b45d890edfee7d4d0
BrowerDevelopments <jcb8561@rit.edu>
2019-05-28 16:17:40 -0400

    Working IK System

--- Assets/Scripts/IK_SubSys/InverseKinematics.cs
+++ Assets/Scripts/IK_SubSys/InverseKinematics.cs
@@ -1,60 +1,89 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 //Quickbooks
 /**
 The class that controlls Inverse Kinematics or linear progression of
 animation.
 @author Brower
  */
- [RequireComponent(typeof(ForwardKinematics))]
 public class InverseKinematics : MonoBehaviour
 {
-    /**
-    This class organizes the code overall putting this IK system's bones pole target and
-    aim bone in one place given the name Bone Information
-     */
     [System.Serializable]
-    public class BoneInformation {
-        [Tooltip("The array of bones Base->Tip base being the bone that hardly moves, tip being the bone that touches the aimTarget is at all possible.")]
-        public Bone[] bones;
-        public Transform poleTarget;
-        public Transform aimTarget;
+    public class Bone {
+        public bool debuggingBone;
+        public int chainLength;
     }
 
-    [Tooltip("This is where all bone properties are set.")]
-    public BoneInformation boneInformation;
-
-    private ForwardKinematics fk;
-
-    void Start() {
-        fk = GetComponent<ForwardKinematics>();
+    [System.Serializable]
+    public class InverseKinematicsVariables {
+        public Transform target;
+        public Transform pole;
+        public int iterations;
+        public float delta = .1f;
     }
 
-    void Update() {
-        fk.ForwardKinematic(boneInformation.bones);
-    }
+    public Bone boneInformation;
+    public InverseKinematicsVariables ikModifiers;
+    protected Quaternion TargetInitialRotation;
+    protected Quaternion EndInitialRotation;
+    protected Transform[] bones;
+    protected float completedLength;
 
-    /**
-    Splices an array at an index +1
-    @idx the index to split the array
-    @arrayToSplice the array to splice at a given index
-     */
-    Bone[] spliceArray(int idx, Bone[] arrayToSplice) {
-        Bone[] res = new Bone[arrayToSplice.Length - idx];
-        for (int i = idx; i < arrayToSplice.Length; i++) {
-            res[i-idx] = arrayToSplice[i];
+    void Awake() {
+        bones = new Transform[boneInformation.chainLength + 1];
+        TargetInitialRotation = ikModifiers.target.rotation;
+        EndInitialRotation = transform.rotation;
+
+        var cur = this.transform;
+        completedLength = 0;
+        for (int i = boneInformation.chainLength - 1; i >= 0; i--) {
+            completedLength += (cur.position - cur.parent.position).magnitude;
+            bones[i+1] = cur;
+            bones[i] = cur.parent;
+            cur = cur.parent;
         }
-        return res;
     }
 
-    /**
-    Just shows where the aim and pole target are
-     */
+    void LateUpdate() {
+        var lastBone = bones[bones.Length-1];
+        for (int iterations = 0; iterations < ikModifiers.iterations; iterations++) {
+            for (int i = bones.Length - 1; i >= 0; i--) {
+                if (i == bones.Length - 1) {
+                    bones[i].rotation = ikModifiers.target.rotation * Quaternion.Inverse(TargetInitialRotation) * EndInitialRotation;
+                } else {
+                    bones[i].rotation = Quaternion.FromToRotation(lastBone.position - bones[i].position, ikModifiers.target.position - bones[i].position) * bones[i].rotation;
+
+                    if (ikModifiers.pole != null && i+2 <= bones.Length - 1) {
+                        Plane plane = new Plane(bones[i+2].position - bones[i].position, bones[i].position);
+                        Vector3 projectedPole = plane.ClosestPointOnPlane(ikModifiers.pole.position);
+                        Vector3 projectedBone = plane.ClosestPointOnPlane(bones[i+1].position);
+
+                        if ((projectedBone - bones[i].position).sqrMagnitude > .01f) {
+                            float angle = Vector3.SignedAngle(projectedBone - bones[i].position, projectedPole - bones[i].position, plane.normal);
+                            bones[i].rotation = Quaternion.AngleAxis(angle, plane.normal) * bones[i].rotation;
+                        }
+                    }
+                }
+
+                if ((lastBone.position - ikModifiers.target.position).sqrMagnitude < ikModifiers.delta * ikModifiers.delta) {
+                    break;
+                }
+            }
+        }
+    }
+
     void OnDrawGizmos() {
-        Gizmos.color = new Color(0, 1, 1, .5f);
-        Gizmos.DrawCube(boneInformation.aimTarget.position, Vector3.one * .1f);
-        Gizmos.color = new Color(1, 1, 0, .5f);
-        Gizmos.DrawCube(boneInformation.poleTarget.position, Vector3.one * .1f);
+        if (boneInformation.debuggingBone) {
+            var cur = this.transform;
+            for (int i = 0; i < boneInformation.chainLength && cur != null && cur.parent != null; i++) {
+                var scale = Vector3.Distance(cur.position, cur.parent.position) * .1f;
+                Handles.matrix = Matrix4x4.TRS(cur.position, Quaternion.FromToRotation(Vector3.up, cur.parent.position - cur.position), new Vector3(scale, Vector3.Distance(cur.parent.position, cur.position), scale));
+                Handles.color = new Color(0, 200, 255, .5f);
+                Handles.DrawWireCube(Vector3.up * .5f, Vector3.one);
+                cur = cur.parent;
+            }
+        }
     }
 }
