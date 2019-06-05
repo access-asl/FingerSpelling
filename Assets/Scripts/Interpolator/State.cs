7d761166ed8afcdc84fbae3386d4291478a9a8b8
Jacob Brower (RIT Student) <jcb8561@mss-2120-06.main.ad.rit.edu>
2019-06-05 19:04:21 -0400

    Begining Saving States

---
+++ Assets/Scripts/Interpolator/State.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+An object to store position, scale, 
+ */
+public class State
+{
+    private Vector3 pos;
+    private Vector3 scale;
+    private Quaternion rot;
+
+    public State(Vector3 pos, Vector3 scale, Quaternion rot) {
+        this.pos = pos;
+        this.scale = scale;
+        this.rot = rot;
+    }
+
+    public Vector3 getPos() { return this.pos; }
+
+    public Vector3 getScale() { return this.scale; }
+
+    public Quaternion getRot() { return this.rot; }
+
+    public void setPos(Vector3 pos) { this.pos = pos; }
+
+    public void setScale(Vector3 scale) { this.scale = scale; }
+
+    public void setRot(Quaternion rot) { this.rot = rot; }
+}
---
+++ Assets/Scripts/Interpolator/StateSaver.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateSaver : MonoBehaviour
+{
+    public Transform objectToManipulate;
+    
+    [System.Serializable]
+    public class Saved {
+        public State[] savedStates;
+    }
+
+    public Saved saved;
+
+    public void saveState(State stateToSave) {
+        State[] newArray = new State[saved.savedStates.Length];
+        for (int i = 0; i < saved.savedStates.Length; i++) {
+            newArray[i] = saved.savedStates[i];
+        }
+        newArray[saved.savedStates.Length] = stateToSave;
+        saved.savedStates = newArray;
+    }
+}
